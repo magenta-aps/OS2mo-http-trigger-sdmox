04e52f328ca7ef2549c1ca52eb38bea34da9fdd6
Emil Madsen <sovende@gmail.com>
2021-04-15 10:56:37 +0200

    Reformatting

--- app/main.py
+++ app/main.py
@@ -22,7 +22,6 @@ from uuid import UUID
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, PlainTextResponse, RedirectResponse
 from os2mo_fastapi_utils.tracing import setup_instrumentation, setup_logging
-
 from structlog.processors import KeyValueRenderer
 
 from app.config import get_settings
